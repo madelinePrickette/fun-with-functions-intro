3f25b546950a4efa0f72a373677a06d668d05434
Madeline Prickette <madelineprickette@gmail.com>
2022-03-17 17:53:29 -0500

    took some notes from codin along with Dev's week 4 video.

---
+++ functions.js
@@ -0,0 +1,38 @@
+console.log( 'thinking... Im hungry!');
+
+function actualMath( num0 , num1 ){
+    console.log('in actualMath:', num0 , num1);
+    let answer = num0 * num1;
+    return answer;
+}
+console.log('Math of 4 & 6:', actualMath( 4 , 6 ));
+//works, end.
+
+
+function logger(){
+    console.log('in logger');
+}
+logger();
+//works, end.
+
+
+function mathExample(){
+    console.log('in mathExample:', );
+    let answer = 4 * 9;
+    return answer;
+}
+console.log('Math of 4 & 9:', mathExample());
+//works, end.
+
+
+function returner(){
+    console.log('in returner:');
+    return true;
+}
+//returner();
+console.log('running returners:', returner());
+//You will see two in returner: 's in the console because the console log runs
+//through the block and console logs, then returns true in the console.
+// we can make returner(); a comment, ill do that now.
+//In the console, now we only see one in returner: and the running returners: true.
+//works, end.
